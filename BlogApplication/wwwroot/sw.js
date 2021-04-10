f246fe59c34057693171ab545615e8b48882a26a
Prashant Dahal <prashantdah41@gmail.com>
2021-04-09 21:45:10 -0400

    Create sw.js and sw-register.js file for registering a service worker and caching files

---
+++ BlogApplication/wwwroot/sw.js
@@ -0,0 +1,28 @@
+﻿const DEFAULT_CACHE = "default_cache-v1";
+
+let urls = [
+    "/",
+    "/css/site.css",
+    "/js/site.js"
+];
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(DEFAULT_CACHE)
+            .then(function (cache) {
+                return cache.addAll(urls);
+            })
+    );
+});
+
+self.addEventListener('fetch', function (event) {
+    event.respondWith(
+        caches.match(event.request)
+            .then(function (response) {
+                // There has been hit in cache so, return the response
+                if (response) {
+                    return response;
+                }
+                return event.request;
+            })
+    );
+});
